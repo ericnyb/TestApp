c7dd8cd0948000a5a78617e29ba0d896ed38b86b
ericnyb <ericnyb@aol.com>
2017-07-17 09:03:42 -0400

    first commit

---
+++ platforms/android/platform_www/plugins/com-moduscreate-plugins-echo/www/ModusEcho.js
@@ -0,0 +1,24 @@
+cordova.define("com-moduscreate-plugins-echo.ModusEcho", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+exports.echo = function(arg0, success, error) {
+    exec(success, error, "ModusEcho", "echo", [arg0]);
+};
+
+exports.echo2 = function (arg0, success, error) {
+    exec(success, error, "ModusEcho", "echo2", [arg0]);
+};
+
+exports.androidcode = function (arg0, success, error) {
+    exec(success, error, "ModusEcho", "androidcode", [arg0]);
+};
+
+exports.getZipFromAirportCode = function (arg0, success, error) {
+    exec(success, error, "ModusEcho", "getZipFromAirportCode", [arg0]);
+};
+
+exports.echojs = function (arg0, success, error) {
+    // Do something
+    success(arg0);
+};
+});
